70675855915b60500cc00f84cc22d5a8136bec8a
chandan <chandan@gmail.com>
2024-05-23 17:34:47 +0530

    fghtyucvb

---
+++ day-12-multi-resourcehandling/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "name" {
+    ami = "ami-013e83f579886baeb"
+    instance_type = "t2.micro"
+    key_name = "keypair"
+    tags = {
+      Name="song"
+    }
+  
+}
+resource "aws_s3_bucket" "name" {
+    bucket = "sdfghjertyuicvbn"
+  
+}
